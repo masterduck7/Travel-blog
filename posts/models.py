7b08233b8e3078cf46154067ec23f8983b610ef2
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2021-01-03 10:55:03 -0300

    Add models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    pub_date = models.DateTimeField(auto_now_add=True, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.title
